40adf690b54d3398d7be83dcfe762f802a904537
DerMonarch <p.lokau@web.de>
2022-03-14 16:37:18 +0100

    inital

---
+++ src/routes/about.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<div class="about">
+    <h2>About Us</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima pariatur molestias eos placeat nulla quam quod, dignissimos mollitia dolores ab minus natus, est adipisci necessitatibus. At officia iusto amet autem recusandae! Ipsam magni, eum temporibus possimus sapiente dolorem earum quia, nobis minus sed sint exercitationem, quos ut assumenda tenetur! Adipisci perferendis animi nobis ipsum suscipit excepturi distinctio aliquid reiciendis error?</p>
+    <a href="/guides">View Guides</a>
+    <a href="/">Home</a>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<div class="index">
+    <h2>Welcome</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod repellendus placeat ipsa corporis explicabo tempore laudantium totam commodi veritatis. Quisquam autem saepe beatae voluptatum veniam corporis possimus, dolorem consequatur eligendi. Minus assumenda quas nisi possimus provident quae consectetur nobis. Iusto doloremque magni, commodi ut officiis perferendis, veniam beatae, ad saepe molestiae ullam fugit repellendus aliquid necessitatibus aut. Voluptatem, veniam quisquam.</p>
+
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+
+</style>
